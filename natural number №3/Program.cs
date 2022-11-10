7a0a2a872caba9137dccba5234d8bd5a06e3bcca
Андрей <@DESKTOP-USER>
2022-11-10 12:50:36 +0400

    Добавьте файлы проекта.

---
+++ natural number №3/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace natural_number__3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          Console.WriteLine("Введите любые три числа:");
+          var S = Console.ReadLine();
+
+          var a = Convert.ToInt32(S[0]);
+          var b = Convert.ToInt32(S[1]);
+          var c = Convert.ToInt32(S[2]);
+
+          int D = a;
+          if ((a < b && b < c) || (a > b && b > c))
+          {    
+            D = b;
+          }
+          else 
+          if ((a > b && a < c) || (a > c && a < b))
+          {
+            D = c;
+          }
+            Console.WriteLine("Среднее число, которое вывелось из них:");
+            Console.WriteLine(D);
+        }
+    }
+}
+    
